b19d1354e71a7a84c3193ef20387cdb6ed1014d3
Fazal Zubair <fazalzubair6573@gmail.com>
2025-02-01 22:23:09 +0500

    Update languageCards.js

--- controllers/languageCards.js
+++ controllers/languageCards.js
@@ -1,80 +1,47 @@
-let languageCards = require("../data/languageCards.json");
+[
+  {
+    "languageFamily": "English",
+    "languageDescription": "English typing skills are essential in today's digital world, as English is the primary language for global communication, business, and technology. Mastering typing in English allows for faster and more efficient communication, increasing productivity in both professional and academic settings. With many online platforms, software, and programming languages using English, it’s a key skill for tech-savvy individuals. Additionally, learning to type accurately and quickly in English enhances cognitive skills like focus and memory. Ultimately, English typing proficiency opens doors to countless opportunities worldwide.",
+    "id": 11,
+    "primaryFontSize": 20,
+    "direction": "ltr",
+    "secondaryFontSize": 18,
+    "requestFont": "Poppins-Regular",
+    "fontURL": null,
+    "supportedLayouts": [
+      { "name": "QWERTY keyboard", "id": 1, " status": 20 },
+      { "name": "AZERTY keyboard", "id": 2, "status": 10 },
+      { "name": "DAVORK keyboard", "id": 3, "status": 10 }
+    ]
+  },
+  {
+    "languageFamily": "Urdu",
+    "languageDescription": "اردو ٹائپنگ سیکھنا آج کے ڈیجیٹل دور میں انتہائی اہمیت رکھتا ہے، خاص طور پر پاکستان اور دیگر اردو بولنے والے ممالک میں۔ اردو میں تیز اور درست ٹائپنگ آپ کو دفتری کاموں، تعلیمی سرگرمیوں اور روزمرہ زندگی میں زیادہ مؤثر اور پیداواری بناتی ہے۔ اردو میں ٹائپنگ سیکھنا آپ کو نہ صرف ٹیکنالوجی کے ساتھ ہم آہنگ کرتا ہے بلکہ زبان کی ترویج اور ترقی میں بھی مددگار ہوتا ہے۔ کمپیوٹر اور انٹرنیٹ پر اردو کی بڑھتی ہوئی مقبولیت کے پیش نظر، اردو ٹائپنگ کی مہارت حاصل کرنا بے حد فائدہ مند ہے۔",
+    "id": 22,
+    "requestFont": "JNNR",
+    "direction": "rtl",
+    "fontURL": null,
+    "primaryFontSize": 25,
+    "secondaryFontSize": 25,
+    "supportedLayouts": [
+      { "name": "Phonetic 1.1", "id": 1, "status": 20 },
+      { "name": "Phonetic 1.0", "id": 23, "status": 20 },
+      { "name": "Phonetic Inpage", "id": 23, "status": 10 }
+    ]
+  },
+  {
+    "languageFamily": "Arabic",
+    "direction": "rtl",
+    "languageDescription": "مهارات الطباعة باللغة العربية أساسية في العالم الرقمي اليوم، حيث تعتبر اللغة العربية لغة أساسية للتواصل في العديد من الدول، كما أنها لغة العمل والتعليم في عدة مجالات. إتقان الطباعة باللغة العربية يساعد على تحسين سرعة التواصل ودقته، مما يزيد الإنتاجية سواء في البيئات المهنية أو الأكاديمية. ومع تطور المنصات الإلكترونية والبرمجيات التي تدعم اللغة العربية، تصبح مهارة الطباعة باللغة العربية ضرورة للمتخصصين في المجال الرقمي. علاوة على ذلك، فإن تعلم الطباعة بدقة وسرعة باللغة العربية يعزز المهارات المعرفية مثل التركيز والذاكرة. في النهاية، يفتح التمكن من الطباعة بالعربية أبوابًا لفرص لا حصر لها في جميع أنحاء العالم العربي.",
+    "id": 18,
+    "primaryFontSize": 20,
+    "secondaryFontSize": 18,
+    "requestFont": "Noto Naskh Arabic",
+    "fontURL": "https://fonts.googleapis.com/css2?family=Noto+Naskh+Arabic:wght@400..700&display=swap",
 
-const fs = require("fs");
-
-const getAllCards = (req, res) => {
-  if (languageCards) {
-    return res.json(languageCards);
-  } else {
-    return res.status(404).send("No Language Cards found.");
+    "supportedLayouts": [
+      { "name": "Arabic (101)", "id": 1, "status": 20 },
+      { "name": "Arabic 102", "id": 2, "status": 10 }
+    ]
   }
-};
-const addCard = (req, res) => {
-  const newCard = req.body;
-  if (newCard) {
-    languageCards.push(newCard);
-    fs.writeFile(
-      "./data/languageCards.json",
-      JSON.stringify(languageCards),
-      (error) => {
-        if (error) {
-          return res.status(404).send("Server Error: " + error);
-        } else {
-          return res.send("New Card has been Added.....");
-        }
-      }
-    );
-  } else {
-    return res.status(404).send("Error in Language Card: " + newCard);
-  }
-};
-
-const singleCard = (req, res) => {
-  const id = req.params.id;
-  if (id) {
-    const card = languageCards.find((card) => card.id === Number(id));
-    if (card) {
-      return res.json(card);
-    } else {
-      return res.status(404).send("No card Available with this id: " + id);
-    }
-  } else {
-    return res.status(404).send("Invalid ID....");
-  }
-};
-const deleteCard = (req, res) => {
-  const id = req.params.id;
-  if (id) {
-    // Check if the card with the specified id exists first
-    const cardExists = languageCards.find((card) => card.id === Number(id));
-
-    if (cardExists) {
-      // Proceed to filter out the card for deletion
-      const updatedCards = languageCards.filter(
-        (card) => card.id !== Number(id)
-      );
-
-      languageCards = updatedCards;
-
-      fs.writeFile(
-        "./data/languageCards.json",
-        JSON.stringify(languageCards),
-        (error) => {
-          if (error) {
-            console.log(error);
-            return res.status(404).send("File not found.");
-          } else {
-            return res.json("Deletion successful for id: " + id);
-          }
-        }
-      );
-    } else {
-      // If the card does not exist, send an appropriate message
-      return res.json("No card available with id: " + id);
-    }
-  } else {
-    return res.status(404).send("Invalid ID....: " + id);
-  }
-};
-
-module.exports = { getAllCards, addCard, deleteCard, singleCard };
+]
