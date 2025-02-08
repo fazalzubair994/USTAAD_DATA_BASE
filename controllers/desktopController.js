a6ea3a924bf0991590e192b6509c0d48b6567b1c
Fazal Zubair <fazalzubair6573@gmail.com>
2025-02-08 14:38:06 +0500

    Desktop Controllers

---
+++ controllers/desktopController.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path"); // Add this line
+
+// Load the user data from the JSON file
+let users = require("../data/DesktopUsers.json");
+
+const getAllUsers = (req, res) => {
+  if (users) {
+    return res.status(201).json({ users: users });
+  } else {
+    return res.status(404).send("No User found.");
+  }
+};
+
+const checkUser = (req, res) => {
+  try {
+    console.log("---------Checking New User-------------------");
+
+    const newUserData = req.body;
+    console.log(JSON.stringify(newUserData, null, 2));
+
+    const { hmid } = newUserData;
+
+    // Check if user already exists
+    const existingUser = users.find((user) => user.hmid === hmid);
+
+    if (existingUser) {
+      console.log("User already exists:", existingUser);
+      return res.json({ message: "User found.", user: existingUser });
+    }
+
+    // If user doesn't exist, add to the users array and save to file
+    users.push(newUserData);
+
+    // Write the updated users array back to the JSON file
+    const filePath = path.join(__dirname, "../data/DesktopUsers.json");
+    fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+
+    console.log("New user added:", newUserData);
+    return res
+      .status(201)
+      .json({ message: "New user added.", user: newUserData });
+  } catch (error) {
+    console.error("An unexpected error occurred:", error);
+    return res.status(500).send("Internal server error.");
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  checkUser,
+};
